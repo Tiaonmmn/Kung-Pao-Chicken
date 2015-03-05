20b23578cc802e77cd22f4806780dd180369353e
huh? <david.sackmary@utexas.edu>
2015-03-04 16:43:40 -0800

    input *.txt

--- New_read_iptables.py
+++ New_read_iptables.py
@@ -2,18 +2,19 @@ import re
 import json
 import collections
 import re
+import glob
 
 #read the text file and generate input, output and chain for special cases 
-def read_Iptables():
+def read_Iptables(filename):
     mylist_input =[]
     mylist_output =[]
     chain = []
     exclude_forward = []
-    text_file = open('hana1.txt', 'r')
+    text_file = open(filename, 'r')
 
     for line in text_file:
         tokens = re.split(r"[' ']+", line)
-        
+        print tokens        
         if len(tokens) < 2:
             continue
         elif tokens[0] == "Chain":
@@ -42,9 +43,9 @@ def read_Iptables():
     return mylist_input, mylist_output,chain, exclude_forward
 
 #finding the speical chain and collect all the firewall rules they have 
-def Find_special_chain(chain):
+def Find_special_chain(filename, chain):
     
-    text_file = open('hana1.txt', 'r')
+    text_file = open(filename, 'r')
     printing = False
     chain_list = []
     name = None
@@ -102,6 +103,7 @@ def merge_special_chain(mylist_input, mylist_output,shash):
     for i in range(len(mylist_input)):        
         if mylist_input[i][3] != "ACCEPT" or mylist_input[i][3] != "REJECT" or mylist_input[i][3] != "LOG" or mylist_input[i][3] != "DROP":
             mylist_input_final.append(mylist_input[i])
+            print mylist_input[i]
             for value in token_hash[mylist_input[i][3]]:
                 if len(value) < 2:
                     continue
@@ -126,8 +128,13 @@ def merge_special_chain(mylist_input, mylist_output,shash):
     return mylist_output_final, mylist_input_final
     
 
-
-
-mylist_input, mylist_output,chain, exclude_forward = read_Iptables()
-shash = Find_special_chain(chain)
-mylist_output_final, mylist_input_final= merge_special_chain(mylist_input, mylist_output,shash)
+files = []
+files = glob.glob("*.txt")
+for n in range(len(files)):
+    if files[n] != "server_list.txt":
+        mylist_input, mylist_output,chain, exclude_forward = read_Iptables(files[n])
+        shash = Find_special_chain(files[n], chain)
+        if not shash:
+            continue
+        else:		    
+            mylist_output_final, mylist_input_final= merge_special_chain(mylist_input, mylist_output,shash)
